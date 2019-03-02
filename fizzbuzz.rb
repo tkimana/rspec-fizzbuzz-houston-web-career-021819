ca05049f0acfc4108a07bd5e491794ae19eaf0e4
Theodore Kimana <kimana.theodore@gmail.com>
2019-03-02 19:40:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,9 @@
-def fizzbuzz(3)
-  puts "Fizz"
-end
+ def fizzbuzz(num)
+  if num % 3 == 0
+    return "Fizz"
+    elsif num % 5 == 0
+      return "Buzz"
+    else num % 3 && 5 == 15
+      return "FizzBuzz"
+ end
+ end
